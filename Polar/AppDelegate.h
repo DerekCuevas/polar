d81276f7daf231e5e109ab69a21d41f3584976e1
DerekCuevas <cuevas@rohan.sdsu.edu>
2014-12-05 13:10:58 -0800

    Initial Commit

---
+++ Polar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Polar
+//
+//  Created by Derek Cuevas on 12/5/14.
+//  Copyright (c) 2014 Derek Cuevas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Polar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Polar
+//
+//  Created by Derek Cuevas on 12/5/14.
+//  Copyright (c) 2014 Derek Cuevas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
